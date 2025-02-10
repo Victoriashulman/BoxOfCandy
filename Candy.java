ecc5477beb79346f2746b21a91de100b44a8202f
Victoriashulman <victorias252@nycstudents.net>
2025-02-10 16:48:28 +0000

    commit

---
+++ Candy.java
@@ -0,0 +1,7 @@
+public class Candy { 
+    private String flavor;
+}
+public Candy(String f)
+{
+    flavor = f;
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-public class Main {}
+public class Main {
+    public static void main(String[] args){
+        Candy[][] box = new Candy[4][3];
+        box[0][1] = new Candy("lime");
+        box[1][1] = new Candy("orange");
+        
+    }
+}
