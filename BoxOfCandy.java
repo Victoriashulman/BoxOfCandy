db835b8113d04dd015cf9d8c01816b5a1a9d82b5
Victoriashulman <victorias252@nycstudents.net>
2025-03-12 15:46:56 +0000

    commit

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -1,22 +1,11 @@
-public class BoxOfCandy{
-    private Candy[][] box;
-    public BoxOfCandy(Candy[][] b)
+public class BoxOfCandy
+{
+private Candy[][] box;
+public BoxOfCandy(Candy[][] b)
     {
         box = b;
     }
     BoxOfCandy b = new BoxOfCandy(box);
     System.out.println(b);
-
-public String toString()
-{
-    String s = "";
-    for(int row = 0; row < box.length; row ++)
-    {
-        for (int col = 0; col < box[0].length; col++)
-        {
-            s += box[row][col] + " ";
-        }
-        s += "\n";
-    }
-    return s;
-}}
+}
+    
--- Candy.java
+++ Candy.java
@@ -1,13 +1,8 @@
 public class Candy { 
-    private String flavor;
+private String flavor;
 public Candy(String f)
 {
     flavor = f;
-}
-box[0][1] = new Candy("lime");
-box[1][1] = new Candy("orange");
-box[2][2] = new Candy("cherry");
-box[3][1] = new Candy("lemon");
-box[3][2] = new Candy("grape");
-System.out.println(box);}}
+}}
+
 
